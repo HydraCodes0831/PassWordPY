92eb9731099b5abe87376c5717c64bc8ebab8434
Jake Stough <59552053+HydraCodes0831@users.noreply.github.com>
2021-07-15 18:39:45 -0400

    Create Password.py

---
+++ Password.py
@@ -0,0 +1,15 @@
+user = input("Please enter a password: ")
+retries = 3
+while user:
+    user_input = input("Please enter your password: ")
+    if user_input == user:
+        print("Password Accepted: ")
+        passWord = True
+        break
+    else:
+        if user_input != user:
+            retries -= 1
+        if retries == 0:
+            print("To many failed attempts, try again later.")
+            break
+        print("Password Incorrect Please Try Again, You Have", retries, "remaining")
